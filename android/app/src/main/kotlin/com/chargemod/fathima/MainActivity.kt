5e14e1618199ac17ccc3b7dca81d13b6c49c48a3
FathimaNooriya <fathimanooriyakn@gmail.com>
2023-12-13 16:26:04 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/chargemod/fathima/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chargemod.fathima
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/domain/core/colors/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColors {
+  Color orange = HexColor("#e28b2d");
+  Color white = HexColor("#2e2e2d");
+  Color black = HexColor("#ffffff");
+  Color darkGray = HexColor("#ebebeb");
+  Color red = HexColor("#d83300");
+  Color green = HexColor("#16d64c");
+  Color yellow = HexColor("#ead907");
+  Color gray = HexColor("#666766");
+  Color lightGray = HexColor("#ebebeb");
+}
